318212b5d478b35a65eb5173098d6438dc9f4b64
Josh Desmond <8055127+JoshDesmond@users.noreply.github.com>
2025-05-11 17:16:16 +0200

    Update Footer Links

--- AutomatiSolutions/src/components/layout/Footer/Footer.tsx
+++ AutomatiSolutions/src/components/layout/Footer/Footer.tsx
@@ -14,14 +14,14 @@ export const Footer: React.FC = () => {
           <div>
             <h4 className="text-sm font-semibold uppercase text-gray-400 tracking-wider">Connect</h4>
             <div className="mt-4 flex space-x-6">
-              <a href="#" className="text-gray-400 hover:text-white">
+              <a href="mailto:developerdesmond@gmail.com" className="text-gray-400 hover:text-white">
                 <span className="sr-only">Email</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                   <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
               </a>
-              <a href="#" className="text-gray-400 hover:text-white">
+              <a href="https://www.linkedin.com/in/joshdesmond/" className="text-gray-400 hover:text-white" target="_blank" rel="noopener noreferrer">
                 <span className="sr-only">LinkedIn</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
